bf33e108f829cb587248283a7c197411a457485a
MelonMayhem2564 <550727@chrome.escg.ac.uk>
2025-03-11 12:10:10 +0000

    map building

---
+++ Assets/Scripts/Player script.cs
@@ -0,0 +1,48 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Playerscript : MonoBehaviour
+{
+    Rigidbody rb;
+    Animator anim;
+    SpriteRenderer sr;
+    float speed = 0.015f;
+    Vector3 movement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Rotation();
+    }
+    void Movement()
+    {
+        if ((Input.GetKey("up") == true || Input.GetKey("w") == true))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        if ((Input.GetKey("down") == true) || (Input.GetKey("s") == true))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+        Rotation();
+    }
+    void Rotation()
+    {
+        if ((Input.GetKey("left") == true) || (Input.GetKey("a") == true))
+        {
+            transform.Rotate(0, -0.5f, 0, Space.Self);
+        }
+        if ((Input.GetKey("right") == true) || (Input.GetKey("d") == true))
+        {
+            transform.Rotate(0, 0.5f, 0, Space.Self);
+        }
+    }
+}
